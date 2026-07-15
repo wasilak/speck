26119a8eac116ceb5ced4358cfb16ed296ee316f
Piotr Boruc <piotr.m.boruc@gmail.com>
2026-07-15 10:42:25 +0200

    [SPECK-24] style: cargo fmt vminitd.rs

--- crates/speck-guest/src/bin/vminitd.rs
+++ crates/speck-guest/src/bin/vminitd.rs
@@ -560,7 +560,9 @@ mod linux {
             if let Some(val) = word
                 .strip_prefix("speck_bind_root=")
                 .filter(|v| !v.is_empty())
-                .filter(|val| val.starts_with('/') && !val.split('/').any(|segment| segment == ".."))
+                .filter(|val| {
+                    val.starts_with('/') && !val.split('/').any(|segment| segment == "..")
+                })
             {
                 return Some(val.to_string());
             }
